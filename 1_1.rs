18c3b5bfab29ed375eebd2599b41edaafa9361cd
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-12-08 16:38:08 +0530

    Done 1_1

---
+++ 1_1.rs
@@ -0,0 +1,40 @@
+use std::io;
+
+fn main(){
+
+    let mut max = (0, -1);
+    let mut index = 0;
+
+    'eachElf : loop{
+        index += 1;
+
+        let mut sum = 0;
+        loop{
+            let mut elem = String::new();
+            match io::stdin().read_line(&mut elem){
+                Ok(num) => num,
+                Err(_) =>  continue,           
+            };
+            elem = elem.trim().to_string();
+            if elem.is_empty(){
+                if sum == 0 {
+                    break 'eachElf
+                }
+                else{
+                    break
+                }
+            }
+            let elem: u32 = elem.trim().parse().expect("Couldnt read as a number");
+            sum += elem;
+
+        }
+        println!("For elf {index}, the amount is {sum}");
+        if sum > max.0 {
+            max.0 = sum;
+            max.1 = index;
+        }
+    }
+    let ans = max.0;
+    println!("{ans}");
+    
+}
